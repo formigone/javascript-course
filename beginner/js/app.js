61a9b2e0416b44e231dd4d075ab2928f1a8dffaf
Rodrigo Silveira <rsilveira@deseretdigital.com>
2017-02-20 19:12:11 -0700

    select tools and colors

--- beginner/js/app.js
+++ beginner/js/app.js
@@ -1,5 +1,13 @@
 function main(options) {
   var canvas = initializeCanvas(options.canvas);
+  var tools = Object.assign({}, { selected: null }, options.tools);
+  var colors = Object.assign({}, { selected: null }, options.colors);
+
+  initializeActions(tools);
+  selectTool(tools, 'pencil');
+
+  initializeColors(colors);
+  selectColor(colors, 'black');
 }
 
 function initializeCanvas(canvas) {
@@ -12,4 +20,43 @@ function initializeCanvas(canvas) {
     ctx: canvas.getContext('2d'),
     canvas: canvas,
   };
+}
+
+function selectTool(tools, action) {
+  tools.selected = action;
+  Object.keys(tools).map(function (tool) {
+    if (tools[tool] instanceof HTMLElement) {
+      tools[tool].classList.remove('active');
+    }
+
+    if (action === tool) {
+      tools[tool].classList.add('active');
+    }
+  });
+}
+
+function initializeActions(tools) {
+  Object.keys(tools).map(function (tool) {
+    if (tools[tool] instanceof HTMLElement) {
+      tools[tool].addEventListener('click', function () {
+        selectTool(tools, tool);
+      });
+    }
+  });
+}
+
+function selectColor(colors, color) {
+  var style = getComputedStyle(colors[color]);
+  colors.selected = color;
+  colors.active.style.background = style.background;
+}
+
+function initializeColors(colors) {
+  Object.keys(colors).map(function (color) {
+    if (colors[color] instanceof HTMLElement && color !== 'active') {
+      colors[color].addEventListener('click', function () {
+        selectColor(colors, color);
+      });
+    }
+  });
 }
