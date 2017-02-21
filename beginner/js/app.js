91d797728037eaa64f3b89dabb54c912c797f0ee
Rodrigo Silveira <rsilveira@deseretdigital.com>
2017-02-20 20:16:40 -0700

    plot points with pencil and brush

--- beginner/js/app.js
+++ beginner/js/app.js
@@ -8,7 +8,51 @@ function main(options) {
 
   initializeColors(colors);
   selectColor(colors, 'black');
+
+  var mouseDown = false;
+  var queue = [];
+
+  canvas.el.addEventListener('mousedown', function (event) {
+    mouseDown = true;
+    var x = event.offsetX;
+    var y = event.offsetY;
+    plot(tools.selected, canvas, x, y);
+  });
+
+  canvas.el.addEventListener('mouseup', function () {
+    mouseDown = false;
+  });
+
+  canvas.el.addEventListener('mousemove', function (event) {
+    if (!mouseDown) {
+      return;
+    }
+
+    var x = event.offsetX;
+    var y = event.offsetY;
+    plot(tools.selected, canvas, x, y);
+  });
 }
+
+  function plot(tool, canvas, x, y) {
+    var kernel = [];
+
+    if (tool === 'pencil') {
+      kernel.push([x, y]);
+    } else if (tool === 'brush') {
+      for (var w = -5; w < 5; w++) {
+        for (var h = -5; h < 5; h++) {
+          kernel.push([x - w, y - h]);
+        }
+      }
+    }
+
+    if (kernel.length > 0) {
+      kernel.map(function (pt) {
+        canvas.ctx.fillRect(pt[0], pt[1], 1, 1);
+      });
+    }
+  }
 
 function initializeCanvas(canvas) {
   var parent = canvas.parentElement;
@@ -16,9 +60,10 @@ function initializeCanvas(canvas) {
   var bodyStyles = getComputedStyle(document.body);
   canvas.width = parent.clientWidth - parseInt(styles.paddingLeft, 10) - parseInt(styles.paddingRight, 10);
   canvas.height = parseInt(bodyStyles.height, 10) - canvas.getBoundingClientRect().top - 20;
+
   return {
+    el: canvas,
     ctx: canvas.getContext('2d'),
-    canvas: canvas,
   };
 }
 
